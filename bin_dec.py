11a4ab329cb4ad09bf9d3f68823435693ad10639
Aleem24 <md.samiulaleem16@gmail.com>
2025-04-05 13:42:12 +0600

    I am done  with my homework Git❤️

---
+++ bin_dec.py
@@ -0,0 +1,5 @@
+binary_num = input("Enter teh Binary Number you want to convert to Decimal:") # Binary number as a string
+
+decimal_num = int(binary_num, 2) # Convert binary string to decimal integer
+
+print(binary_num)
